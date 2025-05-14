e1b1c3ea1c5717dd748974ef959d9eed4a0b4d8a
Suyiyi <75876201+OcrSu@users.noreply.github.com>
2025-05-15 01:51:32 +0800

    Update g-server.py

--- g-server.py
+++ g-server.py
@@ -1408,4 +1408,6 @@ scheduler.start()
 
 
 if __name__ == "__main__":
+      with app.app_context():
+        db.create_all()
     socketio.run(app, host="0.0.0.0", port=8180, allow_unsafe_werkzeug=True)
